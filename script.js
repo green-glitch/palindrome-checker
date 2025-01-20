02e91ada8a0e42a5214b435056ee904d4fbdcf62
Krzysztof Kalinowski <kalinowskik234@gmail.com>
2025-01-20 18:18:21 +0100

    script.js update

--- script.js
+++ script.js
@@ -2,6 +2,14 @@ const textInput = document.getElementById("text-input");
 const checkBtn = document.getElementById("check-btn");
 const result = document.getElementById("result");
 
+const checkPalindrome = () => {
+    let text = textInput.value.toLowerCase();
+    text = text.split(/\s+/).join("");
+    const reversedText = text.split("").reverse().join("");
+    return text === reversedText;
+
+}
+
 checkBtn.addEventListener("click", () => {
-    result.innerText = textInput.value;
+    result.innerText = checkPalindrome() ? `${textInput.value} is palindrome`:`${textInput.value} is not palindrome`;
 });
